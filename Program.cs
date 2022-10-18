496122b861ae53a5ce18b514722b9436c063dc5e
MarcoSydneyHorta <marco.sh@outlook.com>
2022-10-18 15:13:44 -0300

    Initial version

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ShiftNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num, novo;
+
+            Console.Write("Enter numbers separated by space: ");
+            num = Console.ReadLine().Split(" ");
+            Console.Write("Enter the number of shift (between 1 and number of data) : ");
+
+            int k = int.Parse(Console.ReadLine());
+            if (k < 1 || k > num.Length)
+            {
+                Console.WriteLine("Shift number invalid");
+                Console.Read();
+            }
+            else
+            {
+                novo = ShiftNum.Solution(num, k);
+                Console.Write("The number shifted by k to right is : ");
+                for (int i = 0; i < num.Length; i++)
+                {
+                    Console.Write(novo[i]);
+                }
+            }
+        }
+    }
+}
---
+++ ShiftNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftNumber
+{
+    class ShiftNum
+    {
+        static public string[] Solution(string[] arg, int k)
+        {
+            int tam = arg.Length;
+            string[] argus = new string[tam];
+            for (int i = 0; i < tam; i++)
+            {
+                if (i >= k)
+                {
+
+                    argus[i] = arg[i - k];
+                }
+                else
+                {
+                    argus[i] = arg[tam - k + i];
+                }
+            }
+
+            return argus;
+        }
+
+    }
+}
